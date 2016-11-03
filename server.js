b951db34c04c2bfc43ed5474132b9ae73318b575
Andrea R <amrdev08@gmail.com>
2016-11-02 22:32:46 -0400

    prepared

--- server.js
+++ server.js
@@ -15,8 +15,8 @@
   app.get("/campgrounds", function(req,res){
     var campgrounds = [
       {name: "Creek", image: "http://cdn.thepondguy.com/images/homepage/pond-lake-promo.jpg"},
-      {name: "Mountain View", image: "https://upload.wikimedia.org/wikipedia/commons/c/ce/Camping_ground_in_Kippel.jpg"},
-      {name: "Pond", image: "http://www.blm.gov/or/resources/recreation/images/photogallerypics/59_883Whittaker_Creek.JPG"}
+      {name: "Mountain View", image: "http://pop.h-cdn.co/assets/cm/15/05/54c83083b00c9_-_pmx0606pond006_large.jpg"},
+      {name: "Pond", image: "http://openwalls.com/image/264/small_river_1920x1200.jpg"}
       
     ]
     res.render("campgrounds", {campgrounds: campgrounds});
